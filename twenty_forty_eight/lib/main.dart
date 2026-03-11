fd6e48595f133287cca44e3c67d1160075c7e387
Toft <toft.dah@gmail.com>
2026-03-11 12:41:28 +0200

    keep vertical

--- twenty_forty_eight/lib/main.dart
+++ twenty_forty_eight/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'game/bloc/game_bloc.dart';
 import 'game/bloc/game_event.dart';
 import 'game/bloc/game_state.dart';
 import 'game/ui/board_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
